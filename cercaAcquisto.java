59d5341ce2d34e5bd0a72a90383a0118133e0ad3
CARMINEBURTI <127199342+CARMINEBURTI@users.noreply.github.com>
2023-05-10 09:39:48 +0200

    cercaAcquisto.java

--- cercaAcquisto.java
+++ cercaAcquisto.java
@@ -1,9 +1,10 @@
-public Acquisto cerca(Acquisto a){
-int x=-1;
-	for(int i=0;i<lista.length;i++){
-		if(a.getUtente().equals(lista[i].getUtente()) && a.getLista().equals(lista[i].getLista()) && a.getData()==lista[i].getData()){
-			x=i;
-		}
-	}
-return x;
+public int cerca(Acquisto a) {
+    int x = -1;
+    for (int i = 0; i < lista.length; i++) {
+        if (a.getUtente().equals(lista[i].getUtente()) && a.getLista().equals(lista[i].getLista()) && a.getData().equals(lista[i].getData())) {
+            x = i;
+            break;
+        }
+    }
+    return x;
 }
