a0f52db05d7f731c291691eaabf3a7d70c1ebbbd
CARMINEBURTI <127199342+CARMINEBURTI@users.noreply.github.com>
2023-05-10 09:40:30 +0200

    cercaLibro.java

--- cercaLibro.java
+++ cercaLibro.java
@@ -1,9 +1,10 @@
-public Acquisto cerca(Acquisto a){
-int x=-1;
-	for(int i=0;i<lista.length;i++){
-		if(a.getTitolo().equals(lista[i].getTitolo()) &&  a.getAutore().equals(lista[i].getAutore()) && a.getCodice()==lista[i].getCodice()) && a.getPrezzo()==lista[i].getPrezzo()){
-			x=i;
-		}
-	}
-return x;
+public int cerca(Libro a) {
+    int x = -1;
+    for (int i = 0; i < lista.length; i++) {
+        if (a.getTitolo().equals(lista[i].getTitolo()) && a.getAutore().equals(lista[i].getAutore()) && a.getCodice() == lista[i].getCodice() && a.getPrezzo() == lista[i].getPrezzo()) {
+            x = i;
+            break;
+        }
+    }
+    return x;
 }
